40b3cb85db74ee5ca2f8836958e96f5f6821af6b
EByrdS <emmanuel_pajaro@hotmail.com>
2018-10-07 18:59:38 +0000

    Creates POINT before save!

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,10 +1,28 @@
 # frozen_string_literal: true
 
+# :nodoc:
 class Restaurant < ApplicationRecord
   validates :rating, inclusion: { in: [0, 1, 2, 3, 4],
                                   message: '%{value} is not a valid rating' }
+  validates :lat, presence: true
+  validates :lng, presence: true
+  scope :within, lambda { |latitude, longitude, meters_distance|
+    where(format(%{
+     ST_Distance(lonlat, 'POINT(%f %f)') < %d
+    }, longitude, latitude, meters_distance)) # Default unit for geographic type
+  }
+
+  before_save :create_geographic_point
 
   def to_param
     local_id
+  end
+
+  private
+
+  def create_geographic_point
+    return unless lat_changed? || lng_changed?
+
+    self.lonlat = "POINT(#{lng} #{lat})"
   end
 end
