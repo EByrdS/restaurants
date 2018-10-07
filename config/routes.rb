321f3a96c2afb52e67c2577a90f43993427cbfdf
EByrdS <emmanuel_pajaro@hotmail.com>
2018-10-07 18:58:32 +0000

    Adds statistics route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :restaurants, param: :local_id
+  resources :restaurants, param: :local_id do
+    collection do
+      get 'statistics'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
